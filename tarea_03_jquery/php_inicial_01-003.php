c41b3a32c53260009bbd84370a6f48b458d39355
rbn-tena <rbn-tena@gmail.com>
2020-12-22 08:56:36 +0100

    Changes to be committed: 	modified:   tarea_03_jquery/index.html 	modified:   tarea_03_jquery/php_inicial_01-003.php

--- tarea_03_jquery/php_inicial_01-003.php
+++ tarea_03_jquery/php_inicial_01-003.php
@@ -1,35 +1,39 @@
 <?php
-	$server = "localhost";//ubicacion del servidor
-	$user = "root";//usuario
-	$pass = "";//contraseña
-	$BD = "stdcore_practicas";//tabla
-	$conexion = mysqli_connect($server,$user,$pass,$BD);//query de conexion
+	$server = "localhost";
+	$user = "root";
+	$pass = "";
+	$BD = "stdcore_practicas";
+	$conexion = mysqli_connect($server,$user,$pass,$BD);
 	
-	$name= $_POST["name"];//variable nombre obtenida con post
-	$first_last_name = $_POST['first_last_name'];//variable 1er apellido obtenida con post
-	$second_last_name = $_POST['second_last_name'];//variable 2o apellido obtenida con post
-	$phone = (int)$_POST['phone'];//variable telefono obtenida con post
+	$name= $_POST["name"];
+	$first_last_name = $_POST['first_last_name'];
+	$second_last_name = $_POST['second_last_name'];
+	$phone = (int)$_POST['phone'];	
 		
-	$sql = "INSERT INTO php_inicial_ruben (name, first_last_name, second_last_name, phone) VALUES ('$name', '$first_last_name', '$second_last_name', '$phone')";//query de insercion de datos
+	$sql = "INSERT INTO php_inicial_ruben (name, first_last_name, second_last_name, phone) VALUES ('$name', '$first_last_name', '$second_last_name', '$phone')";
 	
-	if (!mysqli_query($conexion, $sql)){echo "no es posible insertar los datos";exit;}//realiza conexion y hace solicicitud / en caso de no obtener respuesta da aviso
+	if (!mysqli_query($conexion, $sql)){echo "no es posible insertar los datos";exit;}
 		
-	$sql = "SELECT * FROM `php_inicial_ruben` ";//query de solicitud de toda la tabla
+	$sql = "SELECT * FROM `php_inicial_ruben` ";
 		
-	$resultado=mysqli_query($conexion, $sql);//variable que realiza la conexion y solicita datos 
+	$resultado=mysqli_query($conexion, $sql);
 	
-	while ($fila = mysqli_fetch_array($resultado)) {//mysqli_fetch_array guarda en el array fila los valores de cada fila creando indices asociativos con el nombre de los campos como claves mientras no devuelva null (se ha optado por usar los indices numericos)
-		
-		$name_tbl = $fila[0];//array que almacena nombres
-		$first_last_name_tbl = $fila[1];//array que alamacena 1eros apellidos
-		$second_last_name_tbl = $fila[2];//array que almacena 2os apellidos
-		$phone_tbl = $fila[3];//array que almacena telefonos 
-		
-		$table[] = array("name_tbl"=>$name_tbl,"first_last_name_tbl"=>$first_last_name_tbl,"second_last_name_tbl"=>$second_last_name_tbl,"phone_tbl"=>$phone_tbl);//añadimos al array table los array anteriores con sus sus indices asociativos
+	//$table = new stdClass();
 
+	//$table 
+	
+	//while ($fila = mysqli_fetch_row($resultado)) {
+	while ($fila = mysqli_fetch_array($resultado)) {
+		
+		$name_tbl = $fila[0];
+		$first_last_name_tbl = $fila[1];
+		$second_last_name_tbl = $fila[2];
+		$phone_tbl = $fila[3];
+		
+		$table[] = array("name_tbl"=>$name_tbl,"first_last_name_tbl"=>$first_last_name_tbl,"second_last_name_tbl"=>$second_last_name_tbl,"phone_tbl"=>$phone_tbl);
 	}
 
-	$json_table =json_encode($table);//convierte $table en el objeto json $json_table
-	echo $json_table;//manda el objeto $json_table a index.html
-	//mysql_close($conexion);//cierra la conexion con db (ERROR si lo hago no llega el json a index)
+	$json_table =json_encode($table);
+	echo $json_table;
 ?>	
+
