42aeff554237285d01ca7df6ff86280c6e19a07f
rbn-tena <rbn.tena@gmail.com>
2021-01-05 10:09:02 +0100

    210105/mñn - añadido create, añadido read, pendiente de eliminar necesidad nuevo en create y añadir sin resultados a read

--- practica_2/read.php
+++ practica_2/read.php
@@ -2,6 +2,50 @@
 	include './db_conf.php';
 	
 	$sql = 'SELECT * FROM `schedule` ';
+	$contador_where=0;
+	if( !empty($_POST['name']) ){
+		if ($contador_where > 0){
+			$where .= " AND name = '".$_POST['name']."'";
+		}
+		$where ="WHERE name = '".$_POST['name']."'";
+		$contador_where +=1;
+	}
+	
+	if( !empty($_POST['first_last_name']) ){
+		if ($contador_where > 0){
+			$where .= " AND first_last_name = '".$_POST['first_last_name']."'";
+		}
+		$where ="WHERE first_last_name = '".$_POST['first_last_name']."'";
+		$contador_where +=1;
+	}
+	
+	if( !empty($_POST['second_last_name']) ){
+		if ($contador_where > 0){
+			$where .= " AND second_last_name = '".$_POST['second_last_name']."'";
+		}
+		$where ="WHERE second_last_name = '".$_POST['second_last_name']."'";
+		$contador_where +=1;
+	}
+	
+	if( !empty($_POST['phone']) ){
+		if ($contador_where > 0){
+			$where .= " AND phone = '".$_POST['phone']."'";
+		}
+		$where ="WHERE phone = '".$_POST['phone']."'";
+		$contador_where +=1;
+	}
+	
+	if( !empty($_POST['email']) ){
+		if ($contador_where > 0){
+			$where .= " AND phone = '".$_POST['email']."'";
+		}
+		$where ="WHERE email = '".$_POST['email']."'";
+		$contador_where +=1;
+	}
+	
+	if ($contador_where > 0){
+		$sql .= $where;
+	}
 		
 	$resultado=mysqli_query($conexion, $sql);
 	
@@ -19,3 +63,4 @@
 	$json_table =json_encode($table);
 	echo $json_table;
 ?>	
+
