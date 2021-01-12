649580cfcf897c64e5956103f21a6adba4af0883
Alex STD <alex.garcia@stdcore.com>
2021-01-12 10:36:21 +0100

    helpers JSON

--- practica_01/tarea_03/php_inicial_01-003.php
+++ practica_01/tarea_03/php_inicial_01-003.php
@@ -1,43 +1,61 @@
 <?php
 
 include_once('../config/db_conf.php');
-$insert = false;
+$err = '';
 
 // CHECK REQUEST:
-if (empty($_POST)) { $error[]= '<p>No se han enviado datos</p>'; $insert++; }
-if (!isset($_POST['name'], $_POST['first_last_name'], $_POST['second_last_name'], $_POST['phone'])) { $error[] = '<p>No se han recibido todos los datos requeridos</p>'; $insert++; }
+if (empty($_POST)) { $error += '<p>No se han enviado datos</p>'; }
+if (!isset($_POST['name'], $_POST['first_last_name'], $_POST['second_last_name'], $_POST['phone'])) { $error += '<p>No se han recibido todos los datos requeridos</p>'; }
 
 // CHECK name:
-if (empty($_POST['name'])) { $error[] = '<p>Campo nombre no puede estar vacío</p>'; $insert++; }
-if (!strlen($_POST['name']) < 40) { $error[] = '<p>Nombre de máximo 40 caracteres</p>'; $insert++; }
+if (empty($_POST['name'])) { $error += '<p>Campo nombre no puede estar vacío</p>'; }
+if (!strlen($_POST['name']) < 40) { $error += '<p>Nombre de máximo 40 caracteres</p>'; }
 
 // CHECK name_last_1:
-if (empty($_POST['first_last_name'])) { $error[] = '<p>Campo primer apellido no puede estar vacio</p>'; $insert++; }
-if (!strlen($_POST['first_last_name']) < 40) {  $error[] = '<p>Primer apellido de máximo 40 caracteres</p>'; $insert++; }
+if (empty($_POST['first_last_name'])) { $error += '<p>Campo primer apellido no puede estar vacío</p>'; }
+if (!strlen($_POST['first_last_name']) < 40) {  $error += '<p>Primer apellido de máximo 40 caracteres</p>'; }
             
 // CHECK name_last_2:
-if (empty($_POST['second_last_name'])) { $error[] = '<p>Campo primer apellido no puede estar vacio</p>'; $insert++; }
-if (!strlen($_POST['second_last_name']) < 40) {  $error[] = '<p>Segundo apellido de máximo 40 caracteres</p>'; $insert++; }
+if (empty($_POST['second_last_name'])) { $error[] += '<p>Campo primer apellido no puede estar vacío</p>'; }
+if (!strlen($_POST['second_last_name']) < 40) { $error[] += '<p>Segundo apellido de máximo 40 caracteres</p>'; }
 
 // Check phone
-if (!empty($_POST['phone'])) { $error []= '<p>Campo telefono no puede estar vacio</p>'; $insert ++; }
-if (strlen($_POST['phone']) != 9) { $error[] = '<p>Campo telefono deben ser 9 cifras</p>'; $insert++; }
-if (!ctype_digit($_POST['phone'])) { $error[] = '<p>Campo telefono deben ser numeros</p>'; $insert ++; }
-      
-if ($insert != 0) { die("ERROR EN DATOS, DEVOLVER JSON con ERROR y mostrarlo en View. No seguir ejecutando."); exit; }
+if (!empty($_POST['phone'])) { $error += '<p>Campo teléfono no puede estar vacío</p>'; }
+if (strlen($_POST['phone']) != 9) { $error += '<p>Campo teléfono deben ser 9 cifras</p>'; }
+if (!ctype_digit($_POST['phone'])) { $error += '<p>Campo teléfono deben ser números</p>'; }
+
+if (!empty($error)) { send_err(-1, $error); exit; }
 
 if (!mysqli_query($conexion, "
     INSERT INTO php_inicial_ruben (name, first_last_name, second_last_name, phone)
     VALUES ('".$_POST['name']."', '".$_POST['first_last_name']."', '".$_POST['second_last_name']."', '".$_POST['phone']."')")
-) { $error[] = '<p>No es posible insertar los datos</p>'; exit; }
+) { send_err(-2, '<p>No es posible insertar los datos</p>'); exit; }
 
 
 $resultado = mysqli_query($conexion, "SELECT * FROM php_inicial_ruben");
 
 while ($fila = mysqli_fetch_array($resultado)) { $table[] = array('name_tbl'=>$fila[0],'first_last_name_tbl'=>$fila[1],'second_last_name_tbl'=>$fila[2],'phone_tbl'=>$fila[3]); }
-$table[] = $error;
-$json_table =json_encode($table);
-echo $json_table;
+
+die(json_encode($json_table));
 
 
-/*--END FILE--*/ ?>
+// HELPERS ----
+// include_once('archivo helpers');
+
+function send_err ($code, $message){
+
+    /*
+    {
+        response {
+            code: $code
+            message : $message
+        }
+        result {}
+    }
+    */
+    die(json_encode(json)); exit;
+
+}
+
+
+exit; /*--END FILE--*/ ?>
